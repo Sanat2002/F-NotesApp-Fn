c8d4ff60fba2270201dff80b940c0c26b16df684
Sanat Thakur <76841209+Sanat2002@users.noreply.github.com>
2021-11-10 14:54:05 +0530

    Update authentication.dart

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:firebase_auth/firebase_auh.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthenticationService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
